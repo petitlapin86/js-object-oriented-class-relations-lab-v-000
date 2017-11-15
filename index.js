07da8387e3b9a5be2c9f1197ab0b36a119075cad
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-15 20:52:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 
 let store = {drivers: [], passengers: [], trips: []}
-// initialize STORE with key of items and users that each point to an empty array
-
-
-
-let driverId = 0 //driver id starts at zero
 
 let driverId = 0
 class Driver {
@@ -25,41 +20,39 @@ class Driver {
   }
 }
 
-let passengerId = 0 //passenger id starts at zero
-
+let passengerId = 0
 
 class Passenger {
-  constructor(name) {
-    this.id = ++passengerId
+  constructor(name){
     this.name = name
-    //insert Passenger into STORE
+    this.id = ++passengerId
     store.passengers.push(this)
   }
-  Trip(){
-   return store.Trip.filter((trip)=> {
-     return trip.passengerId == this.id
-   }) // a passenger goes on many trips. this filters through trips and returns those that match
-}
-drivers(){
-  return this.Trip().map((trip)=> {
-    return trip.driver() // a passenger has many drivers this looks over drivers and returns the ones that match
-  })
-}
+
+  trips(){
+    return store.trips.filter((trip)=> {
+      return trip.passengerId == this.id
+    })
+  }
+  drivers(){
+    return this.trips().map((trip)=> {
+      return trip.driver()
+    })
+  }
 }
 
-
-let tripId = 0  //trip id starts at zero
+let tripId = 0
 class Trip {
-  constructor(Driver, Passenger, startingZip, endingZip) {
+  constructor(driver, passenger, startingZip, endingZip){
     this.driverId = driverId;
     this.passengerId = passengerId;
-    this.id = ++tripId //trip id counts and adds 1 for each new trip
-    store.trips.push(this)   //insert Trip into STORE
+    this.id = ++tripId
+    store.trips.push(this)
   }
   driver(){
-   return store.drivers.find((driver) => { return driver.id === this.driverId })
- } // a trip could have many different drivers this .find connects them
- passenger(){
-   return store.passengers.find((passenger) => { return passenger.id === this.passengerId })
- } // a trip can have many different passengers this .find connects them
+    return store.drivers.find((driver) => { return driver.id === this.driverId })
+  }
+  passenger(){
+    return store.passengers.find((passenger) => { return passenger.id === this.passengerId })
+  }
 }
