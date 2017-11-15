6446f471478e308e92fe5e13be9fc2821c879e3c
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-15 19:51:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 
 let driver = {users: [], items: []}
+// initialize DRIVERS? with key of items and users that each point to an empty array
 
-let userId = 0 
+
+
+let userId = 0
 
 class driver {
   constructor(name) {
     this.id = ++userId
-    this.name = name 
+    this.name = name
 
 
 
-    //insert driver into DRIVER? 
+    //insert driver into DRIVER?
     driver.users.push(this)
   }
 }
